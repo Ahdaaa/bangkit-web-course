462dbcdee9e84dbbedb5799f8bd74d5ac34dc85b
Ahdaaa <afilzag@gmail.com>
2024-03-06 18:36:19 +0700

    feat: final desktop web

--- Dicoding-1/public/script.js
+++ Dicoding-1/public/script.js
@@ -1,3 +1,5 @@
+/* landing page transition */
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -11,3 +13,48 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.transition')
 hiddenElements.forEach((el) => observer.observe(el));
+
+/* dropdown category */
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+
+        caret.classList.toggle('caret-rotate');
+
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            const selectedOption = option.innerText;
+            selected.innerText = selectedOption;
+
+            select.classList.remove('select-clicked');
+
+            caret.classList.remove('caret-rotate');
+
+            menu.classList.remove('menu-open');
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+
+            option.classList.add('active');
+
+            document.querySelectorAll('article').forEach(article => {
+                article.classList.add('hidden');
+            });
+
+            document.getElementById(selectedOption.toLowerCase()).classList.remove('hidden');
+        });
+    });
+});
